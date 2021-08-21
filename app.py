d768b33a1a63fe4e9ec42ea89b9a9200b770543b
Rumee Ahmed <rumeeahmad@gmail.com>
2021-08-21 17:29:20 +0100

    weather data is being retrieved from the response object and returned to the client.

--- app.py
+++ app.py
@@ -15,8 +15,9 @@ def get_weather(city: str) -> dict:
     """
     api_key = os.environ.get('API_KEY')
     url = 'https://api.openweathermap.org/data/2.5/forecast?'
-    response = requests.get(f'{url}q={city}&APPID={api_key}')
+    response = requests.get(f'{url}q={city}&APPID={api_key}&units=metric')
     response.raise_for_status()
+    print(response.json())
     return response.json()
 
 
@@ -31,14 +32,19 @@ def ping():
         "status": "ok",
         "version": "1.0.0"
     }
-    return jsonify(ping=ping_dict)
+    return jsonify(ping_dict)
 
 
 @app.route('/forecast/<city>')
 def forecast(city: str):
-    get_weather(city)
-
-    # return jsonify(ping=ping_dict)
+    weather_data = get_weather(city)
+    weather_dict = {
+        f'{weather_data["list"][0]["weather"][0]["main"].lower()}': f'{weather_data["list"][0]["weather"][0]["description"]}',
+        'humidity': f'{weather_data["list"][0]["main"]["humidity"]}%',
+        'pressure': f'{weather_data["list"][0]["main"]["pressure"]} hPa',
+        'temperature': f'{weather_data["list"][0]["main"]["temp"]}C',
+    }
+    return jsonify(weather_dict)
 
 
 if __name__ == '__main__':
