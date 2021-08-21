9af03551b093a96acbc047462ca1557382248ae0
Rumee Ahmed <rumeeahmad@gmail.com>
2021-08-21 16:59:28 +0100

    The environment variable is being loaded. The forecast route is now calling get_weather which will make the call for a particular city.

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from flask import Flask, jsonify, render_template, request
 from dotenv import load_dotenv
+import requests
+import os
 
+load_dotenv()
 app = Flask(__name__)
+
+
+def get_weather(city: str) -> dict:
+    api_key = os.environ.get('API_KEY')
+    url = 'https://api.openweathermap.org/data/2.5/forecast?'
+    response = requests.get(f'{url}q={city}&APPID={api_key}')
 
 
 @app.route('/ping')
@@ -16,6 +25,13 @@ def ping():
         "version": "1.0.0"
     }
     return jsonify(ping=ping_dict)
+
+
+@app.route('/forecast/<city>')
+def forecast(city: str):
+    get_weather(city)
+
+    # return jsonify(ping=ping_dict)
 
 
 if __name__ == '__main__':
