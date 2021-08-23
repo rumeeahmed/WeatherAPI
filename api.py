70d4d0f9d366490c8724c4a1da3258153615f04a
Rumee Ahmed <rumeeahmad@gmail.com>
2021-08-23 21:20:29 +0100

    Added new routes to the api

--- api.py
+++ api.py
@@ -1,14 +1,13 @@
-from flask import Flask, request, jsonify
+from flask import Flask
 from flask_restful import Api
+from routes.ping import Ping
+from routes.forecast import Forecast
 
 app = Flask(__name__)
 api = Api(app)
 
-
-
-
-
 api.add_resource(Ping, '/ping')
+api.add_resource(Forecast, '/forecast/<city>')
 
 if __name__ == '__main__':
     app.run(debug=True, port=5005)
