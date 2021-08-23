09c70389df1a6d2769873d2e3764289fbf46df72
Rumee Ahmed <rumeeahmad@gmail.com>
2021-08-23 22:03:53 +0100

    loading the environment variables from here.

--- api.py
+++ api.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_restful import Api
-from routes.ping import Ping
 from routes.forecast import Forecast
+from routes.ping import Ping
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 api = Api(app)
+load_dotenv()
 
 api.add_resource(Ping, '/ping')
 api.add_resource(Forecast, '/forecast/<city>')
