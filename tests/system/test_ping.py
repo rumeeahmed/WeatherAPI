c5532fa98d637b47cf941cca8855e1774a09107d
Rumee Ahmed <rumeeahmad@gmail.com>
2021-09-01 19:04:05 +0100

    Added custom error messages to the tests.

--- tests/system/test_ping.py
+++ tests/system/test_ping.py
@@ -6,16 +6,18 @@ class PingTest(SystemBase):
     """
     Object that system tests the Ping route for the weather API.
     """
-    def test_ping_status(self):
+    def test_ping_status(self) -> None:
         """
         Check that the status code returned from this endpoint is 200.
         :return: None
         """
         with self.test_client() as test_client:
             response = test_client.get('/ping')
-            self.assertEqual(response.status_code, 200)
+            self.assertEqual(
+                200, response.status_code, f'Expected the status code to be 200, got {response.status_code} instead.'
+            )
 
-    def test_ping_response(self):
+    def test_ping_response(self) -> None:
         """
         Check that the json returned from the ping endpoint is equal to the value that is expected.
         :return: None
@@ -23,4 +25,7 @@ class PingTest(SystemBase):
         with self.test_client() as test_client:
             response = test_client.get('/ping')
             expected_data = {"name": "weatherservice", "status": "ok", "version": "1.0.0"}
-            self.assertDictEqual(json.loads(response.data), expected_data)
+            self.assertDictEqual(
+                expected_data, json.loads(response.data),
+                'The JSON returned from the endpoint is not equal to the expected JSON'
+            )
